5e4054aeb5fc0e2106ec7788a0bf4400f587feee
cruzmiguelglobe <zmgcruz@globe.com.ph>
2020-06-24 12:48:40 +0000

    created new api for acceptable rides

---
+++ return_acceptable_rides/app.py
@@ -0,0 +1,33 @@
+import json
+import boto3
+
+client = boto3.client('dynamodb', region_name="ap-southeast-1")
+
+def lambda_handler(event, context):
+
+    #rideId = event['pathParameters']["rideId"]
+#
+    #response = client.query(
+    #    TableName = "frab_revalida",
+    #    IndexName = "frab_inverted_index",
+    #    Select = "SPECIFIC_ATTRIBUTES",
+    #    ProjectionExpression="#S",
+    #    ExpressionAttributeNames={
+    #        "#S" : "state"
+    #    },
+    #    KeyConditionExpression="SK = :rideId",
+    #    ExpressionAttributeValues={
+    #        ":rideId": {
+    #            "S": "RIDE#" + rideId
+    #        }
+    #    }
+    #)
+#
+    #state = response["Items"][0]["state"]["S"]
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "response" : "Available Rides"
+        })
+    }
